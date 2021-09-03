9eee942034479d723cef5069b44c7639cecf1a57
Riccardo Marotti <riccardo.marotti@gmail.com>
2021-09-03 20:38:22 +0200

    Add pretty labeling to eight queens

--- eight-queens/db.pl
+++ eight-queens/db.pl
@@ -13,8 +13,25 @@ safe([Queen|Rest]) :-
   safe_queen(Rest, Queen, 1),
   safe(Rest).
 
+print_row(_, 9) :- nl.
+print_row(Queen, Queen) :-
+  write("Q"),
+  Count is Queen + 1,
+  print_row(Queen, Count).
+print_row(Queen, Distance) :-
+  write(" "),
+  Count is Distance + 1,
+  print_row(Queen, Count).
+
+print_queens([]).
+print_queens([Queen|Queens]) :-
+  print_row(Queen, 1),
+  print_queens(Queens), !.
+
 queens(Queens) :-
   length(Queens, 8),
   Queens ins 1..8,
   all_different(Queens),
-  safe(Queens).
+  safe(Queens),
+  label(Queens),
+  print_queens(Queens).
